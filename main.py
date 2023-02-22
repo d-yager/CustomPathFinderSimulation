fbe10d62e14621812134daeea9a02f827fd0ad22
d-yager <davidyager02@gmail.com>
2023-02-22 01:04:12 -0500

    Add functional markers
    Player markers added, so they cans move around in the window freely. Changed how the window background is displayed. Deleted printing of mouse cursor position while drawing.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import pygame
 import os
 
+BACKGROUND = pygame.image.load("images/background.png")
+
 WIDTH = 640
 HEIGHT = 480
 
@@ -18,10 +20,62 @@ window.blit(canvas, (0, 0))
 pygame.display.update()
 
 
+class PlayerMarker:
+    IMG = pygame.image.load("images/blip.png").convert()
+
+    def __init__(self, x, y):
+        self.img = self.IMG
+        self.rect = self.img.get_rect()
+        self.x = x
+        self.y = y
+        self.speed = 0.1
+        self.moveX = 0
+        self.moveY = 0
+
+    def movement_check(self, up=False, down=False, left=False, right=False):
+        self.moveX = 0
+        self.moveY = 0
+        if up and not down:
+            self.moveY = -self.speed
+            print(up)
+        if down and not up:
+            self.moveY = self.speed
+        if right and not left:
+            self.moveX = self.speed
+        if left and not right:
+            self.moveX = -self.speed
+
+        self.x += self.moveX
+        self.y += self.moveY
+
+    def draw(self, win):
+        new_image = self.img
+        new_rect = new_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(new_image, new_rect.topleft)
+        pygame.display.update()
+
+
 def main():
     print("Custom Path Finder Simulation")
     draw()
     save_image()
+    player = PlayerMarker(12, 12)
+    playing = True
+    while playing:
+        update_display(window, player)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+                break
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_w]:
+            player.movement_check(up=True)
+        if pressed_keys[pygame.K_a]:
+            player.movement_check(left=True)
+        if pressed_keys[pygame.K_s]:
+            player.movement_check(down=True)
+        if pressed_keys[pygame.K_d]:
+            player.movement_check(right=True)
 
 
 def draw():
@@ -40,7 +94,6 @@ def draw():
             elif event.type == pygame.MOUSEMOTION:
                 if drawing_mode:
                     current_mouse_pos = pygame.mouse.get_pos()
-                    print(current_mouse_pos)
                     pygame.draw.lines(canvas, color, True, [(current_mouse_pos[0] - 1,
                                                              current_mouse_pos[1] - 1), current_mouse_pos], 8)
                     window.blit(canvas, (0, 0))
@@ -56,7 +109,12 @@ def save_image():
     image_to_save.fill((255, 255, 255, 0))
     image_to_save.blit(window, (0, 0))
     image_to_save.set_colorkey((255, 255, 255))
-    pygame.image.save(image_to_save, "map.png")
+    pygame.image.save(image_to_save, "images/map.png")
+
+
+def update_display(win, player):
+    win.blit(BACKGROUND, (0, 0))
+    player.draw(win)
 
 
 if __name__ == "__main__":
